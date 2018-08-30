69fe3d4a4e239bf0e875644b56cadaf3615a688d
Unknown <javi.saad.wedding@gmail.com>
2018-08-30 19:22:12 +0200

    Floating Language Change

--- js/strings-loader.js
+++ js/strings-loader.js
@@ -9,6 +9,12 @@ function MoveCover(){
     $(".half-move").toggleClass("half-move-to-left")
 }
 
+function SetInnerText(inner, text){
+    inner = text;
+}
+
+
+
 function SetLanguage(lang){
     var url = "json/strings-" + lang + ".json";
     var items = [];
@@ -23,9 +29,8 @@ function SetLanguage(lang){
         for(i = 0; i < texts.length; i++) {
             for(j = 0; j < items.length; j++) {
                 if (items[j].id == texts[i].id){
-                    console.log("id: "+items[j].id+" | text: " + items[j].text);
-
                     texts[i].innerHTML = items[j].text;
+                    break;
                 }
             }
         }
