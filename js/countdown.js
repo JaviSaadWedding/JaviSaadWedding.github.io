ce9cfb6604b524e199415495d3983ed33851a724
Unknown <drouet.dimitri@gmail.com>
2018-08-30 10:27:46 +0200

    Language Selection Page Flags and Font update

--- js/countdown.js
+++ js/countdown.js
@@ -9,7 +9,7 @@ let countDown = new Date('July 31, 2019 14:00:00').getTime(),
       let now = new Date().getTime(),
           distance = countDown - now;
 
-      document.getElementById('days').innerText = Math.floor(distance / (day)),
+        document.getElementById('days').innerText = Math.floor(distance / (day)),
         document.getElementById('hours').innerText = Math.floor((distance % (day)) / (hour)),
         document.getElementById('minutes').innerText = Math.floor((distance % (hour)) / (minute)),
         document.getElementById('seconds').innerText = Math.floor((distance % (minute)) / second);
